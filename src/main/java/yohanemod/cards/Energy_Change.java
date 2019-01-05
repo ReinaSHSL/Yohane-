5ede98cc6c3eb2acfe0411570af6b27b900ae005
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-05 13:30:12 -0800

    Energy Change buff

--- src/main/java/yohanemod/cards/Energy_Change.java
+++ src/main/java/yohanemod/cards/Energy_Change.java
@@ -3,6 +3,7 @@ package yohanemod.cards;
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.animations.TalkAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -17,9 +18,11 @@ public class Energy_Change extends CustomCard {
     private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADED_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     public static final String IMG_PATH = "cards/Energy_Change.png";
     private static final int COST = 0;
     private static final int FALLEN_ENERGY = 4;
+    private static final int ENERGY_AMT = 1;
     private static final int POOL = 1;
     private static final CardRarity rarity = CardRarity.COMMON;
     private static final CardTarget target = CardTarget.SELF;
@@ -29,7 +32,7 @@ public class Energy_Change extends CustomCard {
                 CardType.SKILL, AbstractCardEnum.YOHANE_GREY,
                 rarity, target);
 
-        this.magicNumber = this.baseMagicNumber = FALLEN_ENERGY;
+        this.magicNumber = this.baseMagicNumber = ENERGY_AMT;
     }
 
     public boolean hasEnoughEnergy() {
@@ -46,8 +49,8 @@ public class Energy_Change extends CustomCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         if (p.hasPower(FallenEnergy.POWER_ID) && p.getPower(FallenEnergy.POWER_ID).amount >= this.magicNumber) {
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new FallenEnergy(p, 0), -this.magicNumber));
-            AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.common.GainEnergyAction(1));
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new FallenEnergy(p, 0), -FALLEN_ENERGY));
+            AbstractDungeon.actionManager.addToBottom(new GainEnergyAction(this.magicNumber));
         } else {
             AbstractDungeon.actionManager.addToBottom(new TalkAction(true, "I don't have enough Fallen Energy!", 1.0F, 2.0F));
         }
@@ -63,7 +66,9 @@ public class Energy_Change extends CustomCard {
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.upgradeMagicNumber(-2);
+            this.upgradeMagicNumber(1);
+            this.rawDescription = UPGRADED_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 }
